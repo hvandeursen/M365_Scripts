22739ddb54f69565bec1d0850e1b5f8a7b6b9214
OnzenHans <94694576+hvandeursen@users.noreply.github.com>
2022-12-06 15:59:35 +0100

    Region aanmaken nieuw cert verwijderd

--- M365 EOL unattended connect script.ps1
+++ M365 EOL unattended connect script.ps1
@@ -1,43 +1,17 @@
 # POwershell commando's om een selfsigned certificate te maken wat gebruikt kan worden voor
 # unattended scripting naar Exchange online
 
-# Region Create Certificate
-# Create certificates only once.
 
-
-$newCert = @{
-    DnsName = 'baudevoort-consultancy.nl'
-    CertStoreLocation = 'Cert:\CurrentUser\My'
-    NotAfter = (Get-Date).AddYears(1)
-    KeySpec = 'KeyExchange'
-}
-
-$myCert = New-SelfSignedCertificate @newCert
-
-# Export certificate to .pfx file
-$exportCert = @{
-    FilePath = 'ExoCert.pfx'
-    Password = $(ConvertTo-SecureString -String "!nBR@ndev00rt" -AsPlainText -Force)
-}
-$myCert | Export-PfxCertificate @exportCert
-
-# Export certificate to .cer file
-$myCert | Export-Certificate -FilePath ExoCert.cer
-
-
-
-
-#region USe Certificate
 $connectEXO = @{
-    CertificateFilePath = 'C:\temp\ExoCert.pfx'
-    CertificatePassword = $(ConvertTo-SecureString -String '!nBR@ndev00rt' -AsPlainText -Force)
+    CertificateFilePath = 'C:\Users\hans.vandeursen\OneDrive\Baudevoort Consultancy\Certs\ExoCert.pfx'  
+    CertificatePassword = $(ConvertTo-SecureString -String 'W@chtW00rd!3' -AsPlainText -Force)
     AppID = '394eb93e-137f-4323-b4f8-2c564fceb1d1'
     Organization = 'baudevoortcompany.onmicrosoft.com'
 }
 
 Connect-ExchangeOnline @connectEXO
 
-Connect-AzAccount @connectEXO
+#Connect-AzAccount @connectEXO
 
 
 #Test
