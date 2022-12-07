d10c1011bb022cb1698b535fb43ece57b595f269
OnzenHans <94694576+hvandeursen@users.noreply.github.com>
2022-12-07 08:58:06 +0100

    Created new certificates

--- Azure create UnAttCon certificates.ps1
+++ Azure create UnAttCon certificates.ps1
@@ -19,22 +19,21 @@ Connect-AzureAD
 $currentDate = Get-Date
 $endDate = $currentDate.AddYears(1)
 $notAfter = $endDate.AddYears(1)
-$pwd = "V00r@ccar3"
-$domain = "accare.nl"
+$pwd = "W@chtw00rd!#"
+$domain = "onzenhans.nl"
 $thumb = (New-SelfSignedCertificate -CertStoreLocation cert:\currentuser\my -DnsName $domain -KeyExportPolicy Exportable -Provider "Microsoft Enhanced RSA and AES Cryptographic Provider" -NotAfter $notAfter).Thumbprint
 
-
 $pwd = ConvertTo-SecureString -String $pwd -Force -AsPlainText
-Export-PfxCertificate -cert "cert:\currentuser\my\$thumb" -FilePath c:\temp\certfiles\AccareUAC.pfx -Password $pwd
+Export-PfxCertificate -cert "cert:\currentuser\my\$thumb" -FilePath 'C:\Users\hans.vandeursen\OneDrive\Baudevoort Consultancy\Certs\OnzenHansUAC.pfx' -Password $pwd
     
 # Load the certificate
-$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate("C:\temp\certfiles\AccareUAC.pfx", $pwd)
+$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate("C:\Users\hans.vandeursen\OneDrive\Baudevoort Consultancy\Certs\OnzenHansUAC.pfx", $pwd)
 $keyValue = [System.Convert]::ToBase64String($cert.GetRawCertData())
     
     
 # Create the Azure Active Directory Application
-$application = New-AzureADApplication -DisplayName "Accare AD Unattended connect" -IdentifierUris $domain
-New-AzureADApplicationKeyCredential -ObjectId $application.ObjectId -CustomKeyIdentifier "AccareUAC" -StartDate $currentDate -EndDate $endDate -Type AsymmetricX509Cert -Usage Verify -Value $keyValue
+$application = New-AzureADApplication -DisplayName "Baudevoort AD Unattended connect" -IdentifierUris $domain
+New-AzureADApplicationKeyCredential -ObjectId $application.ObjectId -CustomKeyIdentifier "OnzenHansUAC" -StartDate $currentDate -EndDate $endDate -Type AsymmetricX509Cert -Usage Verify -Value $keyValue
     
 # Create the Service Principal and connect it to the Application
 $sp=New-AzureADServicePrincipal -AppId $application.AppId
@@ -47,15 +46,3 @@ Add-AzureADDirectoryRoleMember -ObjectId $roleId -RefObjectId $sp.ObjectId
 $tenant=Get-AzureADTenantDetail
 # Now you can login to Azure PowerShell with your Service Principal and Certificate
 Connect-AzureAD -TenantId $tenant.ObjectId -ApplicationId  $sp.AppId -CertificateThumbprint $thumb
-
-
-# --------------------
-# SECTION 3
-# --------------------
-# Unattend automation
-$tenantId = "e83fbe90-7c5e-45d0-a497-26ec1b9f8b23"
-$appId = "c431b92f-e073-43e9-9881-8b32c5cf0b6c"
-$thumb = "E4626DAE0F299503347738A490261C429E8EE71C"
-Connect-AzureAD -TenantId $tenantId -ApplicationId  $appId -CertificateThumbprint $thumb
-$users = Get-AzureADUser 
-$users | Format-Table -a
